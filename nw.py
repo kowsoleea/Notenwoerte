37baa97d5bf85a0497e4c9a182ef3839e6583afa
kowsoleea <ikowsoleea@gmail.com>
2015-12-13 22:07:52 +0100

    Now it works

--- nw.py
+++ nw.py
@@ -1,5 +1,5 @@
 ﻿#-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        nw
 # Purpose:
 #
 # Author:      ivan
@@ -12,30 +12,26 @@
 DICT_FILE       =   'german.dic'
 OUT_FILE        =   'woerte.txt'
 
-ALLOWED         =   set(['a', 'h', 'c', 'd', 'e', 'f', 'g'])
+ALLOWED         =   set(['a', 'h', 'c', 'd', 'e', 'f', 'g', 'A', 'H', 'C', 'D', 'E', 'F', 'G'])
 
 def isvalid(word):
-    valid = True
-    i = 0
-    while (valid and  i < len(word)):
-        valid = word[i] in ALLOWED
-        i = i + 1
-    return valid
+	i = 0
+	s = word.lower().replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
+	l = len(s) - 1
+	while ((i < l) and (s[i] in ALLOWED)):
+		i = i + 1
+	return i >= l
 
 def main():
-    infile = open(DICT_FILE, 'r')
-    outfile = open(OUT_FILE, 'w')
-
-    word = infile.readline()
-    print word
-    while word != '':
-        if isvalid(word):
-            outfile.write(word + '\n')
-            print word
-        word = infile.readline()
-
-    infile.close()
-    outfile.close()
+	infile = open(DICT_FILE, 'r')
+	outfile = open(OUT_FILE, 'w')
+	word = infile.readline()
+	while word != '':
+		if isvalid(word):
+			outfile.write(word)
+		word = infile.readline()
+	infile.close()
+	outfile.close()
 
 if __name__ == '__main__':
     main()
